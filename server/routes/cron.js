a6b4171099401cfe35fe63dedcf83af1f8e8794b
LAPTOP-803H1ARJ\user <ginobee33665@gmail.com>
2023-05-26 00:10:26 +0800

    test: 測試crons job

--- server/routes/cron.js
+++ server/routes/cron.js
@@ -3,7 +3,12 @@ const router = express.Router()
 const initDB = require('../service/initDB')
 
 router.get('/', async (req, res, next) => {
-  res.json('test')
+  try {
+    const result = await initDB()
+    res.status(200).json({ success: false, message: result })
+  } catch (err) {
+    res.status(500).json({ success: false, message: err })
+  }
 })
 
 module.exports = router
