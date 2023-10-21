66c4fee6298b9e653469db0693a04e6d711bd5a0
LAPTOP-803H1ARJ\user <ginobee33665@gmail.com>
2023-10-21 17:13:20 +0800

    chore: 測試修改netlify寫法

--- server/netlify/functions/api.js
+++ server/netlify/functions/api.js
@@ -1,14 +1,4 @@
 const serverless = require('serverless-http')
-const express = require('express')
-// const app = require('../../app')
+const app = require('../../app')
 
-// module.exports.handler = serverless(app)
-
-const api = express()
-
-const router = express.Router()
-router.get('/hello', (req, res) => res.send('Hello World!'))
-
-api.use('/api/', router)
-
-export const handler = serverless(api)
+export const handler = serverless(app)
