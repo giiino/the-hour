e34efa1e65d1c29a90a169ab3eb1a0e4d49e4ebe
LAPTOP-803H1ARJ\user <ginobee33665@gmail.com>
2024-03-26 23:55:42 +0800

    feat: cron job新增密碼

--- server/routes/cron.js
+++ server/routes/cron.js
@@ -4,6 +4,9 @@ const initDB = require('../service/initDB')
 
 router.get('/', async (req, res, next) => {
   try {
+    if (req.query.pwd !== process.env.CRON_JOB_PWD) {
+      res.status(500).json({ success: false, message: err })
+    }
     await initDB()
     res.status(200).json({ success: true, message: 'update!' })
   } catch (err) {
